550bd621571a30e62be30b53fca7c541b4fba394
wang <121493993@qq.com>
2019-12-30 22:00:33 +0800

    向数据库增加数据

--- src/main/java/com/wyh/userprofile/controller/StuController.java
+++ src/main/java/com/wyh/userprofile/controller/StuController.java
@@ -4,10 +4,12 @@ import com.wyh.userprofile.pojo.Student;
 import com.wyh.userprofile.repo.StuRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("student")
@@ -20,5 +22,28 @@ public class StuController {
     public List<Student> findAllStu(){
         List<Student> list = stuRepo.findAll();
         return list;
+    }
+
+    /**
+     * 根据sid找学生
+     */
+    @RequestMapping("findStuBySid")
+    public Student findStuBySid(String sid){
+        Optional<Student> stu = stuRepo.findById(sid);
+        return stu.get();
+    }
+
+    /**
+     *添加一个用户
+     */
+    @RequestMapping("saveStu/{sid}/{sname}/{age}/{gender}")
+    public Student saveStu(@PathVariable String sid,@PathVariable String sname,@PathVariable Integer age,@PathVariable String gender){
+        Student stu = new Student();
+        stu.setSid(sid);
+        stu.setSname(sname);
+        stu.setAge(age);
+        stu.setGender(gender);
+        Student save = stuRepo.save(stu);
+        return save;
     }
 }
